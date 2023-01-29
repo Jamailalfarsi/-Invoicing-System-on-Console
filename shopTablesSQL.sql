980308517641d9610fcfa43eb525c6a2be4c1999
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-01-29 11:37:05 +0400

    created invoice and product tables

---
+++ shopTablesSQL.sql
@@ -0,0 +1,12 @@
+Use ShopDataBase;
+
+CREATE TABLE invoice( id INTEGER NOT NULL AUTO_INCREMENT, customer_full_name VARCHAR(50),phone_number VARCHAR(20),invoice_date VARCHAR(50),number_of_items int,total_amount int,
+paid_amount VARCHAR(20),balance VARCHAR(20),item_ID int ,PRIMARY KEY(id));
+
+CREATE TABLE product(item_ID int ,item_name VARCHAR(20), unit_price VARCHAR(15), quantity VARCHAR(15),qty_amount_price VARCHAR(20),PRIMARY KEY(item_ID) );
+
+select*from invoice;
+select*from product;
+
+
+
