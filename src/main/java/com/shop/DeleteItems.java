61ee0207a6b8647fa228de25d9ce4f243fc9201f
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-01-30 11:32:59 +0400

    create delete class

---
+++ src/main/java/com/shop/DeleteItems.java
@@ -0,0 +1,69 @@
+package com.shop;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Scanner;
+
+
+
+public class DeleteItems {
+
+	public void deletItemData(){ 
+		// Creating the connection using Oracle DB
+	    // Note: url syntax is standard, so do grasp
+	    String url = "jdbc:mysql://localhost:3306/ShopDataBase";
+
+	    // Username and password to access DB
+	    // Custom initialization
+	    String user = "root";
+	    String pass = "root";
+
+	    // Entering the data
+	    Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter id: ");
+		Integer idInput = scanner.nextInt();
+
+		String sql = "delete from product where id ='"+idInput + "'";
+		
+	    
+	    // Connection class object
+	    Connection con = null;
+
+	    // Try block to check for exceptions
+	    try {
+
+	        Driver driver = (Driver) Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+	        // Registering drivers
+	        DriverManager.registerDriver(driver);
+
+	        // Reference to connection interface
+	        con = DriverManager.getConnection(url, user,
+	                pass);
+
+	        // Creating a statement
+	        Statement st = con.createStatement();
+
+	        // Executing query
+	        int m = st.executeUpdate(sql);
+	        if (m >=  1)
+	            System.out.println(
+	                    "deleted successfully : " + sql);
+	        else
+	            System.out.println("deleted failed");
+
+	        // Closing the connections
+	        con.close();
+	    }
+
+	    // Catch block to handle exceptions
+	    catch (Exception ex) {
+	        // Display message when exceptions occurs
+	        System.err.println(ex);
+	    }
+	}
+
+		
+
+	}
